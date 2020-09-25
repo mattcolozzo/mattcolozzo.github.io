22aecd76d877cbac761f6e31d7d7ef92e26c9103
mattcolozzo <mattcolozzo@gmail.com>
2020-09-24 21:12:36 -0400

    typescript update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DO NOT EDIT <code>src/App.js</code> and save to reload.
+          DO NOT EDIT <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
