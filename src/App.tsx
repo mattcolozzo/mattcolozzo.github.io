a2ce4bed45dd6447948f03a08ac475dccaed7568
Matthew <mattcolozzo@gmail.com>
2022-10-13 12:15:43 -0400

    remvoed pics

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import me from './assets/me.PNG'
-import guitar from './assets/guitar.JPG'
-import god from './assets/god.JPG'
-import face from './assets/face.JPG'
-const imgArr = [guitar, god, me, face];
+
 function App() {
-  const [img, changeImg] = useState(face)
   
-  useEffect(() => {    
-    const timer = setInterval(() => { imageChanger()  }, 20000);
-    return () => clearInterval(timer)
-  });
-  function imageChanger() {
-    //intentionally potentially OOB to display the alt text sometimes
-   let ranNum = Math.floor(Math.random() * imgArr.length);
-   changeImg(imgArr[ranNum]);
-  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={img} className="App-logo" alt="the man himself" />
-        <p>
-          things:
-        </p>
-        <ul>
+       
+        <ul style={{listStyleType: 'none'}}>
         <li>
         <p>
-          EMAIL: mattcolozzo@gmail.com
+          mattcolozzo@gmail.com
         </p>
+          </li>
+          <li>
+          <a
+          className="App-link"
+          href="https://the-ambler.bandcamp.com/releases"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          bandcamp
+        </a>
           </li>
         <li>
           <a
@@ -37,19 +31,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Spotify (LIVE RECORDING JUST POSTED)
+          spotify 
         </a>
           </li>
-        <li>
-          <a
-          className="App-link"
-          href="https://the-ambler.bandcamp.com/releases"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Newest Mus
-        </a>
-          </li>
+        
           <li>
           <a
           className="App-link"
@@ -74,11 +59,11 @@ function App() {
           <li>
           <a
           className="App-link"
-          href="https://dice.fm/event/wy9gx-mike-fuller-the-best-around-ambler-milky-bones-lja-10th-apr-the-map-room-at-the-bowery-electric-new-york-tickets"
+          href="https://twitter.com/amblerambler"
           target="_blank"
           rel="noopener noreferrer"
         >
-         tickets for 4/10/22 show at bowery electric
+         twitter
         </a>
           </li>
         </ul>
